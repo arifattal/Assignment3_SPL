7381443dceadc8d6db4704c473fe94f7c338ee3d
arifattal <arifattal@gmail.com>
2022-01-04 12:03:07 +0200

    NonBlockingConnectionHandler design changes

--- src/main/java/bgu/spl/net/srv/NonBlockingConnectionHandler.java
+++ src/main/java/bgu/spl/net/srv/NonBlockingConnectionHandler.java
@@ -51,11 +51,7 @@ public class NonBlockingConnectionHandler<T> implements ConnectionHandler<T> {
                     while (buf.hasRemaining()) {
                         T nextMessage = encdec.decodeNextByte(buf.get());
                         if (nextMessage != null) {
-                            T response = protocol.process(nextMessage);
-                            if (response != null) {
-                                writeQueue.add(ByteBuffer.wrap(encdec.encode(response)));
-                                reactor.updateInterestedOps(chan, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
-                            }
+                            protocol.process(nextMessage);
                         }
                     }
                 } finally {
@@ -120,6 +116,10 @@ public class NonBlockingConnectionHandler<T> implements ConnectionHandler<T> {
 
     @Override
     public void send(T msg) {
+        if (msg != null) {
+            writeQueue.add(ByteBuffer.wrap(encdec.encode(msg)));
+            reactor.updateInterestedOps(chan, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+        }
 
     }
 }
